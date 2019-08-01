169e5e10efc2fd0eec8dd297f2e775fd1df61e51
rahulthawal <iamrahulthawal@gmail.com>
2019-08-01 21:04:59 +0530

    WIP

---
+++ src/components/ProductList.vue
@@ -0,0 +1,42 @@
+<!-- src/components/ProductList.vue -->
+<template>
+  <div>
+    <div class="products">
+      <div class="container">
+        <template v-for="product in products">
+          <product-item :product="product"></product-item>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductItem from "./ProductItem.vue";
+import axios from "axios";
+
+export default {
+  name: "product-list",
+  data() {
+    return {
+      products: []
+    };
+  },
+  mounted: function() {
+    axios
+      .get("http://localhost:8000/fake-api/v1/souledStore/products")
+      .then(result => {
+        this.products = result.data;
+      });
+  },
+  components: {
+    "product-item": ProductItem
+  }
+};
+</script>
+<style>
+.products {
+  background: #f7f8fb;
+  padding: 30px 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Vue from 'vue';
+import './plugins/bootstrap-vue'
+import App from './App.vue';
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
+Vue.use(BootstrapVue);
